f27e98dff49937126916d47384da5eb70787f6c1
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-05-22 22:58:20 +0900

    Add : 카드 내 컴포넌트 추가 후 스타일링 중

--- src/pages/List/MovieCard/MovieCard.js
+++ src/pages/List/MovieCard/MovieCard.js
@@ -1,24 +1,78 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Card, CardMedia, CardHeader } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardMedia,
+  CardHeader,
+  Typography,
+  Rating,
+  Badge,
+} from '@mui/material';
 
 function MovieCard() {
   return (
     <CardBox>
-      <Poster src="https://file2.nocutnews.co.kr/newsroom/image/2022/04/08/202204081311322351_0.jpg" />
-      <Title title="닥터 스트레인지: 대혼돈의 멀티버스" />
+      <Poster
+        component="img"
+        image="https://file2.nocutnews.co.kr/newsroom/image/2022/04/08/202204081311322351_0.jpg"
+      />
+      <Title>닥터 스트레인지: 대혼돈의 멀티버스</Title>
+      <InfoContainer>
+        <MovieInfo>2022 · 미국</MovieInfo>
+        <InfoContainer>
+          <Star size="small" max={1} value={1} readOnly />
+          <Number>
+            <Number>2.5 / </Number>5
+          </Number>
+        </InfoContainer>
+      </InfoContainer>
+      <BadgeContainer>
+        <GenreBadge badgeContent="모험" color="warning" width={32} />
+        <GenreBadge badgeContent={<span>판타지</span>} color="primary" />
+      </BadgeContainer>
     </CardBox>
   );
 }
 
 const CardBox = styled(Card)`
+  padding: 14px;
   width: 270px;
+  height: 460px;
+  border-radius: 8px;
 `;
-const Poster = styled.img`
-  width: 242px;
+const Poster = styled(CardMedia)`
+  width: 100%;
+  border-radius: 8px;
 `;
-const Title = styled(CardHeader)`
+const InfoContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Title = styled(Typography)`
+  margin: 4px 0;
   font-size: 14px;
+`;
+const MovieInfo = styled(Typography)`
+  font-size: 12px;
+`;
+const Star = styled(Rating)`
+  color: #02c685;
+`;
+const Number = styled.span`
+  font-size: 14px;
+`;
+const BadgeContainer = styled(Box)`
+  background-color: yellow;
+  position: relative;
+`;
+const GenreBadge = styled(Badge)`
+  position: absolute;
+  right: 0;
+  width: 41px;
+  color: white;
+  font-size: 10px;
 `;
 
 export default MovieCard;
