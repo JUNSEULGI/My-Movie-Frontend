561d7411c1aec68bf71f653492a519964567f327
ssusa2 <audgns9207@naver.com>
2022-05-28 23:57:37 +0900

    Add: movie detail page

--- src/pages/Movie/Movie.js
+++ src/pages/Movie/Movie.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import MyViewLayout from '../../layout/Layout';
 
 function Movie() {
-  return <div>movie</div>;
+  function MovieContainer() {
+    return (
+      <>
+        <MovieBackGround>
+          <Test>adsas</Test>
+        </MovieBackGround>
+      </>
+    );
+  }
+
+  return <MyViewLayout center={<MovieContainer />} />;
 }
 
 export default Movie;
+
+const MovieBackGround = styled.div`
+  background-color: white;
+  color: white;
+`;
+
+const Test = styled.div`
+  background-color: aqua;
+`;
