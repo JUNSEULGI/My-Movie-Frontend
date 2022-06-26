7d53578c51f46c8163eb03f12b4fa2caae6d3e25
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-06-27 00:55:59 +0900

    feat: create recoil states

---
+++ src/state/index.js
@@ -0,0 +1,4 @@
+import tokenState from './token';
+import reviewState from './review';
+
+export { tokenState, reviewState };
---
+++ src/state/token.js
@@ -0,0 +1,7 @@
+import { atom } from 'recoil';
+
+export default atom({
+  key: 'token',
+  default:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiZXhwIjoxNjU2MDA0MDYzfQ.ky7WKj9qMEf13BgA5Uaq0keu9ZkXeaHOUsOzGQ1eyIY',
+});
