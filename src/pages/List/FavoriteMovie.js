5811e3c159d248d17c5e461e351fbcfbc6a126d2
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-07-07 00:34:12 +0900

    feat: title 추가

--- src/pages/List/FavoriteMovie.js
+++ src/pages/List/FavoriteMovie.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Box, Card, Typography } from '@mui/material';
-import { Poster } from './MovieCard';
 
 const FavoriteMovie = ({ bestMovies }) => {
   return (
     <Container>
       {bestMovies.map(movie => (
         <BestMovie key={movie.id}>
-          <BackgroundPoster img={movie.img} />
-          <Title>닥터스트레인지</Title>
+          <BackgroundPoster img={movie.img}>
+            <Title variant="h3">닥터스트레인지</Title>
+          </BackgroundPoster>
         </BestMovie>
       ))}
     </Container>
@@ -22,10 +22,11 @@ const Container = styled(Box)`
   grid-template-rows: 250px 250px;
   gap: 18px 24px;
 
-  & :first-of-type {
+  & div:first-of-type {
     grid-column: 1 / 3;
     grid-row: 1 / 3;
     padding: 20px;
+    // 자기의 첫번째 div만 선택하는 게 아니라, 자식 요소들에도 첫번째 자식 div 죄다 선택되는 문제
   }
 `;
 
@@ -35,11 +36,19 @@ const BestMovie = styled(Card)`
 `;
 
 const BackgroundPoster = styled.div`
+  display: flex;
+  align-items: flex-end;
   width: 100%;
   height: 100%;
   background-image: url(${props => props.img});
   background-size: cover;
   border-radius: 8px;
+`;
+
+const Title = styled(Typography)`
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
 `;
 
 export default FavoriteMovie;
