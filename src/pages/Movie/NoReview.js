5a2357d0e68fef581855eddc89c5f130ffca084e
ssusa2 <audgns9207@naver.com>
2022-07-10 23:38:27 +0900

    modified: src/pages/Movie/NoReview.js

--- src/pages/Movie/NoReview.js
+++ src/pages/Movie/NoReview.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { movieState, reviewState, savingState } from '../../state';
+import { movieState, reviewState } from '../../state';
 import { useRecoilState, useResetRecoilState } from 'recoil';
 import { Box, Link, Typography } from '@mui/material';
 import { CardContainer } from './CardContainer';
 import { ReviewIcon, FabContainer } from './MyIconButton';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import MyViewModal from '../../components/MyViewModal/MyViewModal';
-import { movieState, reviewState } from '../../state';
-import { useRecoilState, useResetRecoilState } from 'recoil';
 import ReviewBox from '../../components/MyViewModal/ReviewBox';
 
 function NoReview({ title }) {
@@ -26,30 +24,6 @@ function NoReview({ title }) {
     console.log(open);
   };
   return (
-<<<<<<< HEAD
-    <Box sx={{ position: 'relative' }}>
-      <CardContainer style={{ justifyContent: 'center' }}>
-        <NoReviewMent variant="h6">
-          남긴 후기가 없어요!
-          <br />
-          후기를 쓰고 &nbsp;
-          <MyLink onClick={() => setOpen(true)}>{title}</MyLink>를 나의 영화
-          목록에 추가해보세요!
-        </NoReviewMent>
-      </CardContainer>
-      <NoReviewFabContainer>
-        <AddReviewButton onClick={() => setOpen(true)}>
-          {/* <NewReview open={open} setOpen={setOpen} /> */}
-          <AddCircleOutlineIcon fontSize="large" />
-        </AddReviewButton>
-        <MyViewModal
-          open={open}
-          closeModal={closeModal}
-          children={<ReviewBox />}
-        />
-      </NoReviewFabContainer>
-    </Box>
-=======
     <>
       <Box sx={{ position: 'relative' }}>
         <CardContainer style={{ justifyContent: 'center' }}>
@@ -73,7 +47,6 @@ function NoReview({ title }) {
         </NoReviewFabContainer>
       </Box>
     </>
->>>>>>> develop
   );
 }
 
