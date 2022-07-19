9cddd09e165ebecd51c0e7bd94ef852c8cee12a5
ssusa2 <audgns9207@naver.com>
2022-07-20 02:53:20 +0900

    running time 합계

--- src/pages/Mypage/WatchedMovie.js
+++ src/pages/Mypage/WatchedMovie.js
@@ -3,15 +3,22 @@ import styled from '@emotion/styled';
 import { CardContainer } from '../Movie';
 import { BASE_URL } from '../../Modules/API';
 import { MovieRating } from '../Movie';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../state';
+import LinearProgress, {
+  linearProgressClasses,
+} from '@mui/material/LinearProgress';
 
 import { Typography, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import movie from '../../state/movie';
 
 function WatchedMovie() {
   const navigate = useNavigate();
 
   const access_token = localStorage.getItem('access_token');
   const [watched, setWatched] = useState([]);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
 
   useEffect(() => {
     fetch(`${BASE_URL}reviews/list`, {
@@ -24,7 +31,14 @@ function WatchedMovie() {
         setWatched(res.result);
       });
   }, []);
-  console.log(watched);
+
+  console.log(watched.map(el => {}));
+
+  let total_watched = 0;
+  for (let i = 0; i < watched.length; i++) {
+    total_watched += watched[i].movie.running_time;
+  }
+  console.log(total_watched);
 
   const moveMoviePage = id => {
     navigate(`/movie/${id}`);
@@ -33,8 +47,22 @@ function WatchedMovie() {
   if (watched.length == 0) return null;
   return (
     <MypageCardContainer>
-      <SubTitle component="h2">시청한 영화</SubTitle>
-      <TT>
+      <WatchedTime>
+        <Typography
+          variant="h2"
+          sx={{ marginBottom: '20px', strong: { color: 'orange' } }}
+        >
+          {userInfo.nickname}님은 총 <strong>{watched.length}</strong>개의 영화,{' '}
+          <strong> {total_watched}</strong>
+          분을 시청하셨어요!{' '}
+        </Typography>
+        <BorderLinearProgress
+          variant="determinate"
+          value={50}
+        ></BorderLinearProgress>
+      </WatchedTime>
+      <SubTitle variant="subtitle1">시청한 영화</SubTitle>
+      <Box sx={{ display: 'flex' }}>
         {watched.map(watchedMovie => {
           const { rating, movie } = watchedMovie;
           const { id, poster } = movie;
@@ -48,7 +76,7 @@ function WatchedMovie() {
             </Movie>
           );
         })}
-      </TT>
+      </Box>
     </MypageCardContainer>
   );
 }
@@ -62,17 +90,11 @@ const MypageCardContainer = styled(CardContainer)`
   margin: 40px 0 20px 20px;
 `;
 
-const TT = styled.div`
-  display: flex;
-`;
-
 const Movie = styled.div`
   margin-right: 16px;
 `;
 const SubTitle = styled(Typography)`
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
 `;
 
 const PPoster = styled.img`
@@ -91,4 +113,16 @@ const Title = styled.p`
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+`;
+
+const WatchedTime = styled.div``;
+
+const BorderLinearProgress = styled(LinearProgress)`
+  height: 10px;
+  border-radius: 5px;
+  background-color: lightgrey;
+
+  .MuiLinearProgress-barColorPrimary {
+    background-color: orange;
+  }
 `;
